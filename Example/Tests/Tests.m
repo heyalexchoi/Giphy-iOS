463ca3856f7c0f520e3a04f0366802a1736ecd19
Alex Choi <heyalexchoi@gmail.com>
2014-08-19 21:02:09 -0400

    start writing specta tests

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -5,24 +5,39 @@
 //  Created by Alex Choi on 08/19/2014.
 //  Copyright (c) 2014 Alex Choi. All rights reserved.
 //
+#import <AXCGiphy/AXCGiphy.h>
 
 SpecBegin(InitialSpecs)
 
+describe(@"AXCGiphy", ^{
+    [AXCGiphy setGiphyAPIKey:kGiphyPublicAPIKey];
+    setAsyncSpecTimeout(3);
+  
+    it(@"should get search results", ^AsyncBlock {
+        [AXCGiphy searchGiphyWithTerm:@"cows" limit:10 offset:0 completion:^(NSArray *results, NSError *error) {
+            expect(results.count).to.equal(13);
+            expect(results.count).to.equal(10);
+            expect(@"this").to.equal(@"that");
+            done();
+        }];
+    });
+});
 describe(@"these will fail", ^{
 
     it(@"can do maths", ^{
-        expect(1).to.equal(2);
+        expect(1).to.equal(1);
     });
 
     it(@"can read", ^{
-        expect(@"number").to.equal(@"string");
+        expect(@"number").to.equal(@"number");
     });
     
     it(@"will wait and fail", ^AsyncBlock {
-        
+        expect(10).to.equal(10);
+        done();
     });
+    
 });
-
 describe(@"these will pass", ^{
     
     it(@"can do maths", ^{
